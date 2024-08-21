646c1057e751bea8979c2b06800299d3b108b337
Harsh Shastrakar <harshsoftdev011@gmail.com>
2024-08-22 02:25:55 +0530

    feat: Dotenv setup

---
+++ src/config/config.ts
@@ -0,0 +1,16 @@
+import dotenvFlow from 'dotenv-flow'
+
+dotenvFlow.config()
+
+// console.log(process.env)
+
+export default {
+    //Genral
+
+    ENV: process.env.ENV,
+    PORT: process.env.PORT,
+    SERVER_URL: process.env.SERVER_URL,
+
+    //Database
+    DATABASE_URL: process.env.DATABASE_URL
+}
