3de465fea6cbc80f609091584035db1b2eaffd57
Harsh Shastrakar <harshsoftdev011@gmail.com>
2024-08-22 22:42:45 +0530

    feat: Health Routes

--- src/controller/apiController.ts
+++ src/controller/apiController.ts
@@ -1,14 +1,27 @@
- 
 import { NextFunction, Request, Response } from 'express'
 import httpResponse from '../util/httpResponse'
 import responseMessage from '../constant/responseMessage'
 import httpError from '../util/httpError'
+import quicker from '../util/quicker'
+
 export default {
     //api route
 
     self: (req: Request, res: Response, next: NextFunction) => {
         try {
             httpResponse(req, res, 200, responseMessage.SUCCESS)
+        } catch (error) {
+            httpError(next, error, req, 500)
+        }
+    },
+    health: (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const healthData = {
+                application: quicker.getAppicationHealth(),
+                system: quicker.getSystemHealth(),
+                timeStamp: Date.now()
+            }
+            httpResponse(req, res, 200, responseMessage.SUCCESS, healthData)
         } catch (error) {
             httpError(next, error, req, 500)
         }
